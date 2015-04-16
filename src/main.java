a70aa73f8f7761e7903d6c10b1c7cc757ba694ce
asakryukin <asakryukin@gmail.com>
2015-04-16 11:28:55 +0600

    again
    wrong

--- src/main.java
+++ src/main.java
@@ -3,17 +3,29 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 
 public class main {
-
+	private static int n;
+	private static List<List<Integer>> list;
+	private static List<List<Boolean>> truth;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Path path=Paths.get("graph.txt");
 		try {
+			
 			BufferedReader reader =Files.newBufferedReader(path);
-			int n;
+			
 			n=Integer.parseInt(reader.readLine());
+			
+			list = new ArrayList<List<Integer>>(n);
+			truth=new ArrayList<List<Boolean>>(n);
+			int[] functions = new int[n];
 			System.out.print(n);
 			int[][] adj=new int[n][n];
 			int[] values=new int[n];
@@ -28,31 +40,36 @@ public class main {
 			
 			
 			String line;
-			while ((line=reader.readLine())!=null){
-				int s1=line.indexOf(" ");
-				int s2=line.substring(s1+1).indexOf(" ");
-				int s3=line.substring(s2+1).indexOf(" ");
-				s2=s2+s1+1;
-				s3=s3+s2+1;
-				
-				int i1,i2,i3;
-				
-				i1=Integer.parseInt(line.substring(0, s1));
-				i2=Integer.parseInt(line.substring(s1+1, s2));
-				i3=Integer.parseInt(line.substring(s2+1, s3));
-				
-				adj[i1-1][i2-1]=1;
-				values[i1-1]=i3;
+			int i=0;
+			while (i<n){
+				List<Integer> l=new ArrayList<Integer>();
+				if((line=reader.readLine())!=null){
+				int s=line.indexOf(" ");;
+				while((s+1)<line.length()){
+					int s1=line.substring(s+1).indexOf(" ");
+					s1=s1+s+1;
+					l.add(Integer.parseInt(line.substring(s+1, s1)));
+					s=s1;
+					
+				}
 				
 			}
+			i++;
+			list.add(l);
+			}
 			
-			
-			
-			print_graph(adj,n);
+			i=0;
+			while (i<n){
+			if((line=reader.readLine())!=null){
+				int s=line.indexOf(" ");
+				functions[i]=Integer.parseInt(line.substring(0, s));
+			}
+			i++;
+			}
 			
 			int[][][] res=new int[8][3][3];
 			
-			res=construct(values);
+			res=construct(values,functions);
 			
 			int x=8;
 			x=x+2;
@@ -64,64 +81,69 @@ public class main {
 			}
 	}
 	
-	static int[] fnc_1(int[] values){
-		int[] res=new int[values.length];
+	static int fnc_not(int value){
+		int res;
+		
+		res=1-value;
+		
+		return res;
+		
+	}
+	
+	static int fnc_and(int[] values){
 		
 		for(int i=0;i<values.length;i++){
-			res[i]=values[i];
+			
+			if (values[i]==0){
+				return 0;
+			}
 		}
 		
-		res[0]=1-values[1];
 		
-		return res;
+		
+		return 1;
 		
 	}
 	
-	static int[] fnc_2(int[] values){
-		int[] res=new int[values.length];
-		
+	static int fnc_or(int[] values){
 		for(int i=0;i<values.length;i++){
-			res[i]=values[i];
+			
+			if (values[i]==1){
+				return 1;
+			}
 		}
 		
-		res[1]=values[0]*values[2];
 		
-		return res;
+		
+		return 0;
 		
 	}
 	
-	static int[] fnc_3(int[] values){
-		int[] res=new int[values.length];
+	static int fnc(int[] values,int number){
+		int res=-1;
 		
-		for(int i=0;i<values.length;i++){
-			res[i]=values[i];
-		}
-		
-		res[2]=1-values[1];
-		
-		return res;
-		
-	}
-	
-	static int[] fnc(int[] values, int node){
-		int[] res=new int[values.length];
-		
-		switch(node){
-		case 0:{res=fnc_1(values); break;}
-		case 1:{res=fnc_2(values); break;}
-		case 2:{res=fnc_3(values); break;}
+		switch(number){
+		case 0:{res=fnc_not(values[0]); break;}
+		case 1:{res=fnc_and(values); break;}
+		case 2:{res=fnc_or(values); break;}
 		}
 		return res;
 	}
 	
-	static int[][][] construct(int[] values){
-		int d=(int) Math.pow(2, values.length);
+	static int[][][] construct(int[] values, int[] functions){
+		int d=(int) Math.pow(2, n);
+		String[] table=new String[n];
+		
+		for(int i=0;i<n;i++){
+			table[i]="";
+		}
+		
 		int l=values.length;
 		int[][][] res=new int [d][l][l];
 	
 		for(int i=0;i<d;i++){
-			for(int j=0;j<l;j++){
-				for(int k=0;k<l;k++){
+			for(int j=0;j<n;j++){
+				for(int k=0;k<n;k++){
 					res[i][j][k]=-1;
 				}
 			}
@@ -139,8 +161,19 @@ public class main {
 				curr[j]=(int) Math.floor((i-sum)/((int) Math.pow(2, l-j-1)));
 			}
 				
-			
-			
+			for(int j=0;j<n;j++){
+				int nn=list.get(j).size();
+				int[] args=new int[nn];
+				
+				for(int k=0;k<nn;k++){
+					args[k]=curr[list.get(j).get(k)-1];
+				}
+				
+				table[j]=table[j]+fnc(args, functions[j]-1)+" ";
+				
+				
+			}
+			/*
 			for(int j=0;j<l;j++){
 				int[] r=new int[l];
 				r=fnc(curr,j);
@@ -166,8 +199,13 @@ public class main {
 					System.out.print(tmp+" ");
 				}
 			}
+			*/
 			System.out.println();
 			
+		}
+		
+		for(int i=0;i<n;i++){
+			System.out.println(table[i]);
 		}
 		
 		return res;
