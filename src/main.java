f788a23ea02130bc1e87915c89099ebb126953d8
asakryukin <asakryukin@gmail.com>
2015-04-21 11:46:51 +0600

    errpor

--- src/main.java
+++ src/main.java
@@ -4,10 +4,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
-import java.util.ListIterator;
+import java.util.TreeMap;
 
 
 public class main {
@@ -16,7 +14,8 @@ public class main {
 	private static List<List<Boolean>> truth;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Path path=Paths.get("graph.txt");
+		Path path=Paths.get("graph.txt"); 
+		
 		try {
 			
 			BufferedReader reader =Files.newBufferedReader(path);
@@ -43,7 +42,7 @@ public class main {
 			int i=0;
 			while (i<n){
 				List<Integer> l=new ArrayList<Integer>();
-				if((line=reader.readLine())!=null){
+				if((line=reader.readLine())!=null && line.length()>2){
 				int s=line.indexOf(" ");;
 				while((s+1)<line.length()){
 					int s1=line.substring(s+1).indexOf(" ");
@@ -61,7 +60,7 @@ public class main {
 			i=0;
 			while (i<n){
 				List<Boolean> l=new ArrayList<Boolean>();
-			if((line=reader.readLine())!=null){
+			if((line=reader.readLine())!=null && line.length()>2){
 				int s=-1;
 				while((s+1)<line.length()){
 					int s1=line.substring(s+1).indexOf(" ");
@@ -91,55 +90,193 @@ public class main {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-	}
-	
-	static int fnc_not(int value){
-		int res;
 		
-		res=1-value;
+		int[][] adj=new int[n][n];
 		
-		return res;
+		for(int i=0;i<n;i++){
+			for(int j=0;j<n;j++){
+				adj[i][j]=0;
+			}
+		}
+		int[] neighbours={2};
+		int[] in=new int[n];
+		/*
+		add_node(adj,1,in);
+		in[0]=1;
 		
-	}
-	
-	static int fnc_and(int[] values){
+		add_node(adj,3,in);
+		in[1]=3;
+		int[] neighbours2={1,3};
+		add_node(adj,2,in);
+		in[2]=2;
+		//delete_node(adj, 1, in);*/
 		
-		for(int i=0;i<values.length;i++){
-			
-			if (values[i]==0){
-				return 0;
+		if(if_there_is_a_cycle(adj)){
+			System.out.println("YES");
+		}else{
+			System.out.println("NO");
+		}
+		
+		List<Integer> feedback_vertex_set=new ArrayList<Integer>();
+		int k=0;
+		for(int i=0;i<n;i++){
+			add_node(adj,i+1,in);
+			if(if_there_is_a_cycle(adj)){
+				feedback_vertex_set.add(i);
+				delete_node(adj, i+1, in);
+			}else{
+				in[k]=i+1;
+				k++;
 			}
 		}
 		
-		
-		
-		return 1;
+		for(int i=0;i<feedback_vertex_set.size();i++){
+			System.out.println(""+(feedback_vertex_set.get(i)+1));
+		}
 		
 	}
+
 	
-	static int fnc_or(int[] values){
-		for(int i=0;i<values.length;i++){
-			
-			if (values[i]==1){
-				return 1;
+	static boolean if_there_is_a_cycle(int[][] adj){
+		boolean con=true;
+		int i=0;
+		
+		int[] labels=new int[adj.length];
+		
+		for(int j=0;j<adj.length;j++){
+			if(has_nodes(adj,j)){
+			labels[j]=0;}
+			else{
+				labels[j]=-1;
 			}
 		}
 		
+		int color=1;
 		
-		
-		return 0;
+		while(i<adj.length ){
+			if(labels[i]==0){
+				
+				if(do_dfs(adj,labels,i,color)){
+					return true;
+				}else{
+					color++;
+				}
+				
+				
+			}
+			i++;
+		}
+			return false;
 		
 	}
 	
-	static int fnc(int[] values,int number){
-		int res=-1;
+	static boolean do_dfs(int[][] adj,int[] labels,int node,int color){
 		
-		switch(number){
-		case 0:{res=fnc_not(values[0]); break;}
-		case 1:{res=fnc_and(values); break;}
-		case 2:{res=fnc_or(values); break;}
+		
+		int i=node;
+		
+		if(has_nodes(adj, i)){
+			for(int j=0;j<adj.length;j++){
+				if(adj[node][j]==1){
+					if(node==j)
+						return true;
+					else {
+						if(labels[j]==0){
+						labels[j]=color;
+						if(do_dfs(adj,labels,j,color)){
+							return true;
+						}
+						color++;
+						}else if (labels[j]==color){
+							return true;
+						}
+					}
+				}
+			}
 		}
-		return res;
+		
+		labels[node]=color;
+		
+		return false;
+		
+	}
+	
+	static boolean has_nodes(int[][] adj,int node){
+		
+		for(int i=0;i<adj.length;i++){
+			if(adj[node][i]==1)
+				return true;
+		}
+		return false;
+	}
+	
+	static void add_node(int[][] adj,int node, int[] in){
+		
+		transpose(adj);
+		
+		for(int i=0;i<adj.length;i++){
+			if(i!=node-1)
+			{
+			for(int k=0;k<in.length;k++){
+				if(in[k]==(i+1)){
+					for(int j=0;j<list.get(i).size();j++){
+						if(node==list.get(i).get(j)){
+							adj[i][node-1]=1;
+						}
+					}
+					break;
+				}
+			}
+			}
+		}
+		
+		for(int i=0;i<list.get(node-1).size();i++){
+			for(int j=0;j<in.length;j++){
+				if(in[j]==list.get(node-1).get(i)){
+					adj[node-1][in[j]-1]=1;
+					break;
+				}else if((node-1)==list.get(node-1).get(i)-1){
+					adj[node-1][node-1]=1;
+					break;
+				}
+			}
+		}
+		transpose(adj);
+	}
+	static void delete_node(int[][] adj,int node, int[] in){
+		transpose(adj);
+		
+		for(int i=0;i<adj.length;i++){
+			if(i!=node-1)
+			{
+			for(int k=0;k<in.length;k++){
+				if(in[k]==(i+1)){
+					for(int j=0;j<list.get(i).size();j++){
+						if(node==list.get(i).get(j)){
+							adj[i][node-1]=0;
+						}
+					}
+					break;
+				}
+			}
+			}
+		}
+		
+		for(int i=0;i<adj.length;i++){
+			
+					adj[node-1][i]=0;
+					
+
+		}
+		transpose(adj);
+	}
+	
+	static void detect_cycles(int[][] adj,int[] in){
+		int[] inside=new int[in.length]; 
+		for(int i=0;i<in.length;i++){
+			inside[i]=in[i];
+		}
+		
 	}
 	
 	static void construct(int[] values){
@@ -168,7 +305,7 @@ public class main {
 			table[i]=table[i]+":";
 			
 			for(int j=0;j<n;j++){
-				
+				if (list.get(j).size()>0){
 				int neighbours_number=list.get(j).size();
 				List<Integer> neighbours=list.get(j);
 				int[] neighbour_values=new int[neighbours_number];
@@ -194,7 +331,7 @@ public class main {
 					}
 					table[i]=table[i]+" ";
 				}
-				
+			}
 			}
 		}
 		
@@ -217,4 +354,16 @@ public class main {
 			}
 			System.out.println();
 	}
+	public static void transpose(int [][] x){
+        int[][] t = new int[x[0].length][x.length];
+        for (int i = 0; i < x.length; i++){
+            for (int j = 0; j < x[0].length; j++)
+                t[i][j] = x[i][j];
+        }
+        for (int i = 0; i < x.length; i++){
+            for (int j = 0; j < x[0].length; j++)
+                x[j][i] = t[i][j];
+        }
+       
+    }
 }
